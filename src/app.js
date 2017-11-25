bb9f52db5d4def2c71190e195d04d369112e4dca
marcpre <marcus.presich@gmail.com>
2017-11-25 13:03:56 +0100

    refactor app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require('dotenv-safe').load()
 const express = require('express')
 
 const app = express()
@@ -5,20 +6,30 @@ const path = require('path')
 const server = require('http').createServer(app)
 const axios = require('axios')
 const querystring = require('querystring')
-
-require('dotenv').config()
-
 const bodyParser = require('body-parser')
+const logger = require('morgan')
 
+process.on('uncaughtException', err =>
+  console.error('uncaught exception: ', err))
+process.on('unhandledRejection', (reason, p) =>
+  console.error('unhandled rejection: ', reason, p))
+
+app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false,
+}))
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/views/index.html`))
+  res.sendFile('index.html')
 })
 
 const instance = axios.create({
   baseURL: 'https://api.imgur.com/3/',
-  headers: { Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}` },
+  headers: {
+    Authorization: `Client-ID ${process.env.IMGUR_CLIENT_ID}`,
+  },
 })
 
 app.get('/search/:query', (req, res) => {
@@ -32,13 +43,16 @@ app.get('/search/:query', (req, res) => {
     })
 })
 
-app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
-app.use('/public', express.static(path.join(__dirname, 'public')))
+// app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+app.use(path.join(`${__dirname}/../public`), express.static(path.join(__dirname, 'public')))
 
 if (process.env.NODE_ENV !== 'production') {
   require('reload')(server, app)
 }
 
+const port = process.env.APP_PORT || 8080
+const host = process.env.APP_HOST || 'localhost'
+
 server.listen(process.env.PORT, () => {
-  console.log('Listening on port '.concat(process.env.PORT))
+  console.log(`Listening on ${host}:${port}`)
 })
