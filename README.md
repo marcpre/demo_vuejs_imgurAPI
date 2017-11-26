4fde65dab71e1fa2cb42bc91fb6bad04f973090c
marcpre <marcus.presich@gmail.com>
2017-11-26 12:38:29 +0100

    adding imgur images

--- README.md
+++ README.md
@@ -37,4 +37,26 @@ Syntax: `{{item.title}}`
 
 [Vue Directives](https://012.vuejs.org/guide/directives.html)
 
+## Resources
 
+The plugin for Vue.js provides services for making web requests and handle responses using a XMLHttpRequest or JSONP.
+Use in html `    <script src="/vue-resource/dist/vue-resource.js"></script>  `
+
+Use in code:
+
+```
+ this.$http.get('/someUrl').then(response => {
+
+    // get body data
+    this.someData = response.body;
+
+  }, response => {
+    // error callback
+  });
+```
+
+[Documentation](https://github.com/pagekit/vue-resource)
+
+## v-bind
+
+`v-bind:src` binds a javascript property to the src
--- public/script.js
+++ public/script.js
@@ -1,21 +1,62 @@
+const PRICE = 9.99
+
 new Vue({
   el: '#app', // where Vue should be applied
-  data: { //all properties here are available in the DOM
-    total: 3,
-    items: [{
-        title: 'Item 1'
-      },
-      {
-        title: 'Item 2'
-      },
-      {
-        title: 'Item 3'
-      }
-    ]
+  data: { // all properties here are available in the DOM
+    total: 0,
+    items: [],
+    cart: [],
+    search: ''
   },
   methods: {
+    onSubmit: function() {
+      this.$http
+      .get('/search/'.concat('90s'))
+      .then(function(res) {
+        this.items = res.data
+      })
+    },
     addItem: function (index) {
-      this.total += 10
+      let item = this.items[index];
+      let found = false;
+      for (var i = 0; i < this.cart.length; i++) {
+        if (this.cart[i].id === item.id) {
+          found = true;          
+          this.cart[i].qty++;
+          break
+        }
+      }
+      if (!found) {
+        this.cart = this.cart.concat({
+          id: item.id,
+          title: item.title,
+          qty: 1,
+          price: PRICE
+        });
+      }
+      this.total += PRICE;
+    },
+    inc: function (item) {
+      item.qty++
+        this.total += PRICE
+    },
+    dec: function (item) {
+      item.qty--
+      this.total -= PRICE      
+        if (item.qty <= 0) {
+          for (let y = 0; y < this.cart.length; y++) {
+            if (this.cart[y].id === item.id) {
+              this.cart.splice(y, 1)
+              break
+            }
+          }
+        }
+      this.total -= PRICE
+    },
+  },
+  filters: {
+    currency: function (price) {
+      return `$`.concat(price.toFixed(2))
     }
   }
 })
